c85cf8c770d32503876f7b3e0b7197eb1ac5ae0e
Tia Nguyen <tia.nkm03@gmail.com>
2025-05-28 17:45:41 +0100

    add webview and tidy code

--- src/extension.ts
+++ src/extension.ts
@@ -2,30 +2,26 @@ import * as vscode from "vscode"
 import getCarbonIntensity from "./services/carbonIntensity"
 
 let carbonStatusBarItem: vscode.StatusBarItem
+let latestCarbonData: any = null
 
 export function activate(context: vscode.ExtensionContext) {
   // Create status bar
-  console.log('Congratulations, your extension "vscarbon" is now active!')
+  vscode.window.showInformationMessage(`VSCarbon is active`)
   carbonStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100)
-  carbonStatusBarItem.command = "extension.showCarbon"
+  // /clicking on status item will show grid composition details
+  carbonStatusBarItem.command = "vscarbon.showGridDetails"
   context.subscriptions.push(carbonStatusBarItem)
 
-  // Update data every 30'
-  updateCarbonIntensity()
-  setInterval(updateCarbonIntensity, 30 * 60 * 1000)
-
-  // Commands
-  // context.subscriptions.push(
-  //   vscode.commands.registerCommand("vscarbon.showCarbon", () => {
-  //     // vscode.window.showInformationMessage(
-  //     //   `Current carbon intensity: ${carbonStatusBarItem.tooltip}`
-  //     // )
-  //   })
-  // )
+  // Register commands
+  context.subscriptions.push(
+    vscode.commands.registerCommand("vscarbon.showGridDetails", showGridDetails),
+    vscode.commands.registerCommand("vscarbon.showCarbon", showCarbon)
+  )
 }
 async function updateCarbonIntensity() {
-  // hardcode for now
+  // hardcode postcode for now
   const data = await getCarbonIntensity("al10")
+  latestCarbonData = data
   console.log("carbon intensity", data?.intensity)
   if (!data) {
     carbonStatusBarItem.text = "⚡Carbon: Unknown"
@@ -41,7 +37,27 @@ async function updateCarbonIntensity() {
     icon = "😿"
   }
 
-  carbonStatusBarItem.text = `${icon} Carbon intensity: ${data?.intensity}gCO₂/kWh`
-  // carbonStatusBarItem.tooltip = `Carbon intensity: ${data?.intensity}gCO₂/kWh (${data?.index}) in ${data?.region}`
+  carbonStatusBarItem.text = `${icon} Carbon intensity: ${data?.intensity}gCO₂/kWh in ${data.region}`
   carbonStatusBarItem.show()
+}
+
+function showCarbon() {
+  updateCarbonIntensity()
+  setInterval(updateCarbonIntensity, 30 * 60 * 1000)
+}
+
+function showGridDetails() {
+  const panel = vscode.window.createWebviewPanel(
+    "gridDetails",
+    "Grid's details",
+    vscode.ViewColumn.One,
+    {}
+  )
+  panel.webview.html = `<html>
+      <body>
+        <h1>Grid's Details</h1>
+        <p>pie charts or 24hr forecast</p>
+        <p>${latestCarbonData?.intensity}</p>
+      </body>
+    </html>`
 }
--- src/services/carbonIntensity.ts
+++ src/services/carbonIntensity.ts
@@ -2,21 +2,20 @@ export default async function getCarbonIntensity(postcode?: string) {
   try {
     const url = postcode
       ? `https://api.carbonintensity.org.uk/regional/postcode/${postcode}`
-      : `https://api.carbonintensity.org.uk/regional`
+      : `https://api.carbonintensity.org.uk/regional/england`
 
     const response = await fetch(url)
-
     if (!response.ok) {
       throw new Error(`Error! Status: ${response.status}`)
     }
+
     const data = await response.json()
-    const regionData = (data as any)?.data[0]
-    // console.log("region data", regionData)
-    // console.log(regionData.data[0].intensity.forecast)
+    const regionalData = (data as { data: any[] }).data[0]
+    // console.log(regionalData.data[0].intensity.forecast)
     return {
-      intensity: regionData.data[0].intensity.forecast,
-      index: regionData.data[0].intensity.index,
-      region: regionData.shortname || "UK",
+      intensity: regionalData.data[0].intensity.forecast,
+      index: regionalData.data[0].intensity.index,
+      region: regionalData.shortname || "England",
     }
   } catch (error) {
     console.error("Failed to fetch carbon intensity:", error)
